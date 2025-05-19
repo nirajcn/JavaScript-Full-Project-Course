5333dc0c2f02ebdab085b3cf3eceede6daf79cb6
Niraj Chauhan <niraj323264@gmail.com>
2025-05-19 11:13:35 +0000

    Conversion Opration

---
+++ 01_Basics/03_conversionOpration.js
@@ -0,0 +1,16 @@
+let score = "10";
+// let score = "10abc"; NaN will show In terminal
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+//   let valueInNumber = Number(score)
+//   console.log(typeof valueInNumber)
+
+//   console.log(valueInNumber);
+
+let someNumber = 35;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
