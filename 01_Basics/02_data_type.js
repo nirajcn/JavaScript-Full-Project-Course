d79e2f7cbf52fbd51ff4b954ac33f25354c8bd4b
Niraj Chauhan <niraj323264@gmail.com>
2025-05-19 10:28:28 +0000

    dataTypes

---
+++ 01_Basics/02_data_type.js
@@ -0,0 +1,27 @@
+"use Strict"; // Treat all Js Code in Newer Version
+
+
+// alert("Hello JavaScript")  we are using NodeJs Not Browser
+
+
+let name = "Niraj";
+let age = 22;
+let loggedIN = true;
+
+
+// Number => 2 to Power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null standalone value    
+// undefined => 
+// symbol => unique
+
+
+//object
+
+console.log(typeof "Niraj")
+
+console.log(typeof undefined)
+
+console.log(typeof null)
