e8208c2db899d3c36ce1531ff0d149da79f57b34
Niraj Chauhan <niraj323264@gmail.com>
2025-05-19 15:28:47 +0000

    Strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,17 @@
+const Name = "Niraj";
+const RepoCount = 45;
+
+// console.log(Name + RepoCount + " Value");   //This is old version And Not Good habits
+
+console.log(`Hello My Name is ${Name} and My Repo is ${RepoCount}.`);
+
+const gameName = new String("freefire");
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+
+console.log(gameName.charAt(2));
