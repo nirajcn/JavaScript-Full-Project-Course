fbf857c7f0e7e655d17622f3e53f6ae35433c497
Niraj Chauhan <niraj323264@gmail.com>
2025-05-19 09:46:54 +0000

    done

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 344465;
+var Name = "Niraj";
+var accountEmail = "nirajcn07@gmail.com"
+let Password = 223466;
+accountCity = "Chapra"
+
+// accountId = 34455 Not allowd
+
+console.log(accountId, Name, accountEmail, Password, accountCity);
+
+console.table([accountId, Name, accountEmail, accountCity]);
