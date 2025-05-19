a3585db494f19c02e19c6faf47e3c60c3a7fa552
Niraj Chauhan <niraj323264@gmail.com>
2025-05-19 12:04:02 +0000

    opration

--- 01_Basics/03_conversionOpration.js
+++ 01_Basics/03_conversionOpration.js
@@ -1,4 +1,4 @@
-let score = "10";
+// let score = "10";
 // let score = "10abc"; NaN will show In terminal
 
 // console.log(typeof score);
@@ -9,8 +9,25 @@ let score = "10";
 
 //   console.log(valueInNumber);
 
-let someNumber = 35;
+// let someNumber = 35;
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// **************** Oprations ***************
+
+// let value = 4;
+// let negValue = -value;
+
+// // console.log(negValue);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let gameCounter = 100;
+
+gameCounter++; //use Prefix or Postfix
+console.log(gameCounter);
