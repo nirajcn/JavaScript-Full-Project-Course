5c4f455722da62404200f4bab7c146c01e2092d3
Niraj Chauhan <niraj323264@gmail.com>
2025-05-19 12:51:27 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 2);
+// console.log(2 != 1);
+
+
+// console.log ("3" > 2)
+// console.log ("04" > 2)
+
+
+// console.log (null > 0)
+// console.log (null == 0)
+// console.log (null < 0)
+// console.log (null >= 0)
+
+
+console.log (undefined > 0)
+console.log (undefined == 0)
+console.log (undefined < 0)
