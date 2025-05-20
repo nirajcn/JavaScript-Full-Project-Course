564763e20e8a92cb80cf57fee26d0bdec6e6b3a1
Niraj Chauhan <niraj323264@gmail.com>
2025-05-20 14:57:10 +0000

    Array Done

--- 02_Basics/02_array.js
+++ 02_Basics/02_array.js
@@ -22,10 +22,10 @@ console.log(real_another_array);
 
 console.log(Array.isArray("Niraj"))
 console.log(Array.from("Niraj"))
-console.log(Array.from({name: "Niraj"}))  //inerersting
+console.log(Array.from({ name: "Niraj" }))  //inerersting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
- 
+
 console.log(Array.of(score1, score2, score3));
