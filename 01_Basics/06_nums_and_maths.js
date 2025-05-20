bc59b11e87e57b510bde28b0337a70d28df9b1f6
Niraj Chauhan <niraj323264@gmail.com>
2025-05-20 10:57:10 +0000

    math and num

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,41 @@
+
+// const score = 400;
+// console.log(score);
+
+
+// const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log( balance.toFixed(2));
+
+// const otherNumber = 23.4546
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundred = 1000000;
+// console.log(hundred.toLocaleString()); //default american Value Like 1,000,000 and Indian('en-IN') Value Like 10,00,000
+
+
+// ++++++++++++++++++++++++++++++++++++ Maths  +++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.7));
+// console.log("Max value: ",Math.max(7,4,3,8,6))
+// console.log("Min value: ",Math.min(7,4,3,8,6))
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min);
+
