db50fd38eab3285faa4f3470bb38544ba46345e0
Niraj Chauhan <niraj323264@gmail.com>
2025-06-05 09:34:41 +0000

    update

--- 03_Basics/03_arrow.js
+++ 03_Basics/03_arrow.js
@@ -1,31 +1,38 @@
-const user = {
-    username: "niraj",
-    price: 999,
+// const user = {
+//     username: "niraj",
+//     price: 999,
 
-    wellcomeMessage: function () {
-        console.log(`${this.username} , wellcome to website!`);
-        console.log(this);
+//     wellcomeMessage: function () {
+//         console.log(`${this.username} , wellcome to website!`);
+//         // console.log(this);
 
-    }
+//     }
 
-}
+// }
 
 // user.wellcomeMessage()
-// user.username = "chauhan"
+// user.username = "nirajcn"
 // user.wellcomeMessage()
+
+
+
+
+// console.log(this);   // output: {}
+
 
 
 // function chai(){
 //     let username = "niraj"
-//     console.log(this);
+//     console.log(this.user);
 
 // }
 
 // chai()
 
+// ++++++++++++++++++++++++++++++++ Arrow Function ++++++++++++++++++++++++++++++++++++++++
 
 
-const chai = () => {                // we can use >>>>const chai = function()
+const chai = () => {                // we can use >>> const chai = function()
 
     let username = "niraj"
     console.log(this.username);
@@ -36,16 +43,19 @@ const chai = () => {                // we can use >>>>const chai = function()
 
 // +++++++++++++++++++++++++++++++++++++++++++++++
 
-//Arrow Function  () => {}
-
-// const addTwo = (num1, num2)  =>{
-//     return num1 + num2
+// const addTwo =  (num1, num2) => {
+//     return num1 + num2;
 // }
+
+// const addTwo = (num1, num2) => num1 + num2  // not require here return becouse '{}'
+
+// console.log(addTwo(4,7));
+
 
 // const addTwo = (num1, num2)  =>  num1 + num2
 // const addTwo = (num1, num2)  =>  (num1 + num2)
 
-const addTwo = (num1, num2) => ({username: "niraj"})
+const addTwo = (num1, num2) => ({username: "nirajcn"})
 
 
 console.log(addTwo(3,5));
